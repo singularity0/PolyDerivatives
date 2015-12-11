733a07a02513a71671e4b992b4463a6901cb0b78
jokuf <jokuf2010@gmail.com>
2015-12-11 21:19:26 +0200

    add some logic

--- derivative.py
+++ derivative.py
@@ -1,9 +1,14 @@
-
+import re
 
 class Derivative:
     """docstring for Derivative"""
     def __init__(self, param):
         self.input = param
 
-    def __validator(self):
+    def __validator(self, asd):
         pass
+
+    def __check_for_var(self, polynom):
+        f = re.compile('[/d]+')
+        f.findall(polynom)
+        
